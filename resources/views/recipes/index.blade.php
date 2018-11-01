5fdf8d6fe8268d71e4ae0ef2940ff153ce5c973b
Daniel Pace <d.pacedan@gmail.com>
2018-11-01 17:54:39 +0100

    - Added failover if no recipes are available

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -3,28 +3,28 @@
 @section('title', 'Recipes')
 
 @section('content')
-
-<div class="container bg-white p-4">
-{{-- Loop for each Recipe --}}
-    @foreach ($recipes as $recipe)
-        {{-- <h2></h2>
-        <p>{{$recipe->body}}</p> --}}
-
-        <div class="card-recipe flex-row">
-                <a href="#recipeID" class="float-left card-image-recipe">
-                    <img src="#TODO Recipe Thumbnail" alt="" class="img-fluid image-center">
-                </a>
-                <div class="card-body p-1">
-                    <h3 class="card-title">
-                        <a href="#recipeID" class="link-unstyled">{{$recipe->title}}</a>
-                    </h3>
-                    <p class="card-text pt-1">
-                        #TODO RECIPE SHORT DESCRIPTION
-                    </p>
-                </div>
+    <div class="container bg-white p-4">
+        @if(count($recipes) > 1)
+            {{-- Loop for each Recipe --}}
+            @foreach ($recipes as $recipe)
+                <div class="card-recipe flex-row">
+                        <a href="#recipeID" class="float-left card-image-recipe">
+                            <img src="#TODO Recipe Thumbnail" alt="" class="img-fluid image-center">
+                        </a>
+                        <div class="card-body p-1">
+                            <h3 class="card-title">
+                                <a href="#recipeID" class="link-unstyled">{{$recipe->title}}</a>
+                            </h3>
+                            <p class="card-text pt-1">
+                                #TODO RECIPE SHORT DESCRIPTION
+                            </p>
+                        </div>
+                    </div>
+            @endforeach
+        @else
+            <div class="jumbotron">
+                <h1>Wow, such empty! </h1>
             </div>
-    @endforeach
-</div>
-
-
+        @endif
+    </div>
 @endsection
