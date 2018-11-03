9c1170582f83a221985eff9120fe9689a2491074
Daniel Pace <d.pacedan@gmail.com>
2018-11-03 11:52:36 +0100

    - Added laravelcollective forms

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Custome Service Providers
+         */
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -225,7 +229,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+  
     ],
 
 ];
