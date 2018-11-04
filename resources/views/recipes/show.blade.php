b18adb735608e1e51a623ad84fa4e4a8004a39b5
Daniel Pace <d.pacedan@gmail.com>
2018-11-04 14:24:55 +0100

    - Added dropdown admin options (edit and delete) to show view

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -4,6 +4,7 @@
     {{$recipe->title}}
 @endsection
 
+{{-- Drop Down menu for admin Edit and Delete buttons --}}
 @section('recipe-options')
     <div class="dropdown recipe-options">
         <button class="btn btn-secondary dropdown-toggle"
@@ -22,58 +23,50 @@
 
 @section('content')
     <div class="container mw-sm-md-100 bg-white p-md-4">
-        {{-- check if recipe exists --}}
-        @if($recipe)
-            {{-- Header --}}
-            <div class="row recipe-header px-md-1 py-md-4 bg-info">
-                <div class="col-md flex-grow-0">
-                        <img src="{{--- $recipe->image ---}}" alt="" class="img-fluid recipe-image bg-dark d-block m-auto">
-                </div>
-                <div class="col-md mt-4 text-center text-md-left">
-                    <h3>{{$recipe->title}}</h3>
-                    <p>{{$recipe->body}}</p>
-                </div>
-                @yield('recipe-options')
+        {{-- Header --}}
+        <div class="row recipe-header px-md-1 py-md-4 bg-info">
+            <div class="col-md flex-grow-0">
+                    <img src="{{--- $recipe->image ---}}" alt="" class="img-fluid recipe-image bg-dark d-block m-auto">
             </div>
+            <div class="col-md mt-4 text-center text-md-left">
+                <h3>{{$recipe->title}}</h3>
+                <p>{{$recipe->body}}</p>
+            </div>
+            {{-- Include admin options --}}
+            @yield('recipe-options')
+        </div>
 
-            {{-- Mian Section --}}
-            <div class="row pt-4">
-                {{-- Ingredients --}}
-                <div class="col-md-3 bg-white border-0">
-                    <h3>Ingredients</h3>
-                    <ul class="list-ingredient">
-                        {{-- @forelse ($ingredients as $key => $ingredient)
-                            @if ($key%2)
-                                <li class="list-ingredient-item">{{$ingredient}}</li>
-                            @else
-                                <li class="list-ingredient-item bg-primary">{{$ingredient}}</li>
-                            @endif
-                        @empty
-                            <p>This recippe requires no ingredients.</p>
-                        @endforelse --}}
-                    </ul>
-                </div>
-
-                {{-- Method --}}
-                <div class="col-md bg-white">
-                    <h3 class="card-title">Method</h3>
-                    <ol class="list-method">
-                    {{-- @forelse ($method as $step)
-                        <li class="list-method-items">
-                            {{$step}}
-                        </li>
+        {{-- Mian Section --}}
+        <div class="row pt-4">
+            {{-- Ingredients --}}
+            <div class="col-md-3 bg-white border-0">
+                <h3>Ingredients</h3>
+                <ul class="list-ingredient">
+                    {{-- @forelse ($ingredients as $key => $ingredient)
+                        @if ($key%2)
+                            <li class="list-ingredient-item">{{$ingredient}}</li>
+                        @else
+                            <li class="list-ingredient-item bg-primary">{{$ingredient}}</li>
+                        @endif
                     @empty
-                        <p>No steps available for this recipe.</p>
+                        <p>This recippe requires no ingredients.</p>
                     @endforelse --}}
-                    </ul>
-                </div>
+                </ul>
             </div>
-        {{-- if recipe does not exist --}}
-        @else
-            <div class="jumbotron">
-                <h1 class=>Wow, such empty! </h1>
-                <p>This recipe does not exist, <a href="/about">contact us</a> if you would like to suggest the recipe.</p>
+
+            {{-- Method --}}
+            <div class="col-md bg-white">
+                <h3 class="card-title">Method</h3>
+                <ol class="list-method">
+                {{-- @forelse ($method as $step)
+                    <li class="list-method-items">
+                        {{$step}}
+                    </li>
+                @empty
+                    <p>No steps available for this recipe.</p>
+                @endforelse --}}
+                </ul>
             </div>
-        @endif
+        </div>
     </div>
 @endsection    
