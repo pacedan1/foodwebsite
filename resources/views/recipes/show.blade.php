e301ac9dcc54ea214a2279a38919fe56f549ef45
Daniel Pace <d.pacedan@gmail.com>
2018-11-04 13:00:53 +0100

    Added ingredients and method to recipe blade

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -4,16 +4,28 @@
     {{$recipe->title}}
 @endsection
 
+@section('recipe-options')
+    <div class="dropdown recipe-options">
+        <button class="btn btn-secondary dropdown-toggle"
+                type="button" id="recipeOptions" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+        </button>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="recipeOptions">
+            <a class="dropdown-item" href="/recipes/{{$recipe->id}}/edit">Edit</a>
+            {!! Form::open(['action' => ['RecipesController@destroy', $recipe->id]]) !!}
+                {{ Form::hidden('_method', 'delete') }}
+                {{ Form::submit('Delete', ['class' => 'btn dropdown-item'])}}
+            {!! Form::close() !!}
+        </div>
+    </div>
+@endsection
+
 @section('content')
     <div class="container mw-sm-md-100 bg-white p-md-4">
         {{-- check if recipe exists --}}
         @if($recipe)
-        {!! Form::open(['action' => ['RecipesController@destroy', $recipe->id], 'class' => 'form']) !!}
-            <a href="/recipes/{{$recipe->id}}/edit" class="btn btn-primary">Edit</a>
-            {{ Form::hidden('_method', 'delete') }}
-            {{ Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-        {!! Form::close() !!}
-            <div class="row mx-a p-0 pb-3 px-md-1 py-md-4 bg-secondary">
+            {{-- Header --}}
+            <div class="row recipe-header px-md-1 py-md-4 bg-info">
                 <div class="col-md flex-grow-0">
                         <img src="{{--- $recipe->image ---}}" alt="" class="img-fluid recipe-image bg-dark d-block m-auto">
                 </div>
@@ -21,10 +33,40 @@
                     <h3>{{$recipe->title}}</h3>
                     <p>{{$recipe->body}}</p>
                 </div>
+                @yield('recipe-options')
             </div>
-            <div class="row">
-                <div class="col-3 bg-white">Ingrediants</div>
-                <div class="col bg-white">Method</div>
+
+            {{-- Mian Section --}}
+            <div class="row pt-4">
+                {{-- Ingredients --}}
+                <div class="col-md-3 bg-white border-0">
+                    <h3>Ingredients</h3>
+                    <ul class="list-ingredient">
+                        {{-- @forelse ($ingredients as $key => $ingredient)
+                            @if ($key%2)
+                                <li class="list-ingredient-item">{{$ingredient}}</li>
+                            @else
+                                <li class="list-ingredient-item bg-primary">{{$ingredient}}</li>
+                            @endif
+                        @empty
+                            <p>This recippe requires no ingredients.</p>
+                        @endforelse --}}
+                    </ul>
+                </div>
+
+                {{-- Method --}}
+                <div class="col-md bg-white">
+                    <h3 class="card-title">Method</h3>
+                    <ol class="list-method">
+                    {{-- @forelse ($method as $step)
+                        <li class="list-method-items">
+                            {{$step}}
+                        </li>
+                    @empty
+                        <p>No steps available for this recipe.</p>
+                    @endforelse --}}
+                    </ul>
+                </div>
             </div>
         {{-- if recipe does not exist --}}
         @else
@@ -34,4 +76,4 @@
             </div>
         @endif
     </div>
-@endsection
+@endsection    
